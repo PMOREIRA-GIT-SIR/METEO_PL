eb32b2e2bf2cb52ca72d8e5b5b584d21d7369892
Pedro Moreira <pmoreira.estg@gmail.com>
2013-11-01 20:54:52 +0000

    intro js e jq

--- ClientJSONtest.php
+++ ClientJSONtest.php
@@ -1,6 +1,6 @@
 <?php
 $jsonstr = 
-file_get_contents("http://api.worldweatheronline.com/free/v1/weather.ashx?q=New+York&format=json&num_of_days=3&key=sattd4uyxvb8h223w2hgrwhw");
+file_get_contents("http://api.worldweatheronline.com/free/v1/weather.ashx?q=moncao,Viana+do+Castelo&format=json&num_of_days=3&key=sattd4uyxvb8h223w2hgrwhw");
 //echo($jsonstr);
 
 $json = json_decode($jsonstr);
@@ -10,6 +10,8 @@ $curr_location		= $json->data->request[0]->query;
 $curr_temp			= $json->data->current_condition[0]->temp_C;
 $curr_hum			= $json->data->current_condition[0]->humidity;
 $curr_icon			= $json->data->current_condition[0]->weatherIconUrl[0]->value;
+
+$d3_temp = $json->data->weather[2]->tempMaxC;
 ?>
 
 <html>
@@ -22,6 +24,7 @@ $curr_icon			= $json->data->current_condition[0]->weatherIconUrl[0]->value;
 			<h2>temperatura atual : <?php echo $curr_temp ?></h2>
 			<h2>humidade atual : <?php echo $curr_hum."%" ?></h2>
 			<h2><img src="<? echo $curr_icon; ?>"/></h2>
+			<h2>temp max no dia 3: <?php echo $d3_temp; ?></h2>
 		</section>
 	</body>
 </html>
